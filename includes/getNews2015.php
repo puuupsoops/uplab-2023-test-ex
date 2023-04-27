28bf8c41bfdc0405f979cda7884a9508f4b0264c
VasiliiPolshakov <45201a@gmail.com>
2023-04-27 17:35:09 +0300

    done

---
+++ includes/getNews2015.php
@@ -0,0 +1,45 @@
+<?php
+/** php 8.1 */
+
+// Использую код инфоблока, Вместо id 12 , т.к. могут быть раные идентификаторы при миграции на сервер.
+$i_block_code = 'NEWS';
+
+\Bitrix\Main\Loader::includeModule('iblock') ?? throw new \Exception('Модуль инфоблока не найден.', 500);
+
+$id = \CIBlock::GetList(
+    [],
+    ['CODE' => $i_block_code],
+)->fetch();
+
+// тут будет 55.
+$i_block_id = $id['ID'];
+
+// год выборки
+$filter_years = '2015';
+
+$connection = \Bitrix\Main\Application::getConnection();
+$SITE_DIR = 'http://' . $_SERVER['HTTP_HOST'];
+
+// для данных
+$arData = [];
+
+$sql = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/q.sql');
+
+// region Стучимся в базу
+$result = $connection->query(sprintf($sql, $SITE_DIR, $i_block_id, $filter_years));
+
+while($ar = $result->fetch())
+{
+    // тут битриксовский объект времени
+    $ar['date'] = \FormatDate("d F Y H:i", $ar['date']->getTimestamp());
+    // тут делим так как в базе нет методов
+    $ar['tags'] = explode( ',' , $ar['tags']);
+    $arData[] = $ar;
+}
+
+//endregion
+/** @var object $json_result Ответ в строке JSON */
+$json_response = json_encode($arData);
+header(header: 'Content-Type: application/json', response_code: 200);
+echo $json_response;
+exit;
---
+++ q.sql
@@ -0,0 +1,22 @@
+SELECT e.ID as id,
+       ( REPLACE( REPLACE( REPLACE(i.DETAIL_PAGE_URL , "#SITE_DIR#", "%1$s"), "#SECTION_CODE#", t_section.CODE), "#ELEMENT_CODE#", e.CODE)) as url,
+       CONCAT("%1$s","/upload/",f.SUBDIR, "/", f.FILE_NAME) as image,
+       e.NAME as name,
+       t_section.CODE as sectionName,
+       e.ACTIVE_FROM as date, -- date Дата вида “12 декабря 2015 12:59” (день месяц год часы минуты)
+
+       -- author - автор новости (свойство инфоблока типа “Привязка к элементам с автозаполнением”. Нужно вывести имя)
+        ( SELECT ( SELECT SUBSTRING_INDEX(NAME, " ", 1) FROM b_iblock_element e WHERE PROPERTY_309 = e.ID )
+            FROM b_iblock_element_prop_s%2$s prop
+                WHERE prop.IBLOCK_ELEMENT_ID = e.ID
+        ) as author,
+
+        e.TAGS as tags -- решить с тэгами, массив тегов
+
+FROM b_iblock_element e
+    JOIN b_iblock i ON e.IBLOCK_ID = i.ID
+    JOIN b_file f ON e.PREVIEW_PICTURE = f.ID
+    JOIN b_iblock_section t_section ON e.IBLOCK_SECTION_ID = t_section.ID
+WHERE e.IBLOCK_ID = %2$s
+    AND ( e.ACTIVE_FROM >= "%3$s-01-01 " AND e.ACTIVE_FROM <= "%3$s-12-31") -- по дате
+;
