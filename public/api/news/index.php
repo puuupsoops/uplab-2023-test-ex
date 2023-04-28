4afebc5384878616e9317208be763b7f537a4323
puuupsoops <58659389+puuupsoops@users.noreply.github.com>
2023-04-28 18:18:11 +0300

    Update index.php
    fix: exception

--- public/api/news/index.php
+++ public/api/news/index.php
@@ -1,7 +1,7 @@
 <?php
 try{
-    require_once $_SERVER["DOCUMENT_ROOT"]. '/bitrix/modules/main/include/prolog_before.php';
-    include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/getNews2015.php';
+    require_once $_SERVER["DOCUMENT_ROOT"] . '/bitrix/modules/main/include/prolog_before.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/getNews2015.php';
 }catch (\Exception $e){
     header(header: 'Content-Type: application/json', response_code: 500);
     echo json_encode(['error' => [ 'code' => $e->getCode(), 'message' => $e->getMessage()]]);
